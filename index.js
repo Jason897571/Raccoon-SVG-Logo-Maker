70cf48a084a473249a348c67c7799b9d126dba4a
Jason897571 <king.gs1314@outlook.com>
2024-02-04 17:11:23 -0500

    complete square and circle class

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ const questions = [
 {
     type: 'input',
     message: 'Enter the text in the shape',
-    name: 'text'
+    name: 'text',
+    validate: function(input){
+        if(input.length > 3){
+            return 'Text should be 3 characters or less. Please enter again.';
+        }
+        else if(input.length == 0){
+            return 'Text should not be empty. Please enter again.';
+        }
+        return true;
+    }
 },
 {
     type: 'input',
@@ -45,9 +54,11 @@ inquirer
         else if (answers.shape === 'Circle'){
             const circle = new Circle(answers.shape,answers.shapeColor, answers.text, answers.textColor);
             html = circle.html_generator();
+            save_html(html);
         }
         else if (answers.shape === 'Square'){
             const square = new Square(answers.shape,answers.shapeColor, answers.text, answers.textColor);
             html = square.html_generator();
+            save_html(html);
         }
     })
