4deffd6abc62e7644f16f7f2fa02125c72bf6120
Jason897571 <king.gs1314@outlook.com>
2024-02-04 18:13:39 -0500

    complete basic function and continue on testing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// import class from shapes.js
 const { Circle, Square, Triangle } = require('./lib/shapes');
 
+// questions for users
 const questions = [
 {
     type: 'list',
@@ -11,9 +13,31 @@ const questions = [
 
 },
 {
+    type: 'list',
+    name: 'shapeColorChoice',
+    message: 'Do you want to choose a color or enter a color code as shape color?',
+    choices: ['Choose a color for shape', 'Enter a color code for shape']
+},
+{
+    type: 'list',
+    name: 'shapeColor',
+    message: 'Choose the color of the shape',
+    choices: ['Red','Blue','Green','Yellow','Purple','Orange','Black','White','Pink','Gray'],
+    when: (answers) => answers.shapeColorChoice === 'Choose a color for shape' // question depends on the answer above
+},
+{
     type: 'input',
-    message: 'Enter the color of the shape',
-    name: 'shapeColor'
+    name: 'shapeColor',
+    message: 'Enter the six-digit color code for the shape (e.g., #FFFFFF for white)',
+    validate: function(input) {
+        // validate the six digit color
+        const regex = /^#?[0-9A-Fa-f]{6}$/;
+        if (!regex.test(input)) {
+            return 'Please enter a valid six-digit color code.';
+        }
+        return true;
+    },
+    when: (answers) => answers.shapeColorChoice === 'Enter a color code for shape'
 },
 {
     type: 'input',
@@ -30,35 +54,70 @@ const questions = [
     }
 },
 {
+    type: 'list',
+    name: 'textColorChoice',
+    message: 'Do you want to choose a color or enter a color code as text color?',
+    choices: ['Choose a color for text', 'Enter a color code for text']
+},
+{
+    type: 'list',
+    name: 'textColor',
+    message: 'Choose the color of the shape',
+    choices: ['Red','Blue','Green','Yellow','Purple','Orange','Black','White','Pink','Gray'],
+    when: (answers) => answers.textColorChoice === 'Choose a color for text' // question depends on the answer above
+},
+{
     type: 'input',
-    message: 'Enter the text color',
-    name: 'textColor'
+    name: 'textColor',
+    message: 'Enter the six-digit color code for the shape (e.g., #FFFFFF for white)',
+    validate: function(input) {
+        // validate the six digit color
+        const regex = /^#?[0-9A-Fa-f]{6}$/;
+        if (!regex.test(input)) {
+            return 'Please enter a valid six-digit color code.';
+        }
+        return true;
+    },
+    when: (answers) => answers.textColorChoice === 'Enter a color code for text'
 }
 ]
 
-save_html = (html) => {
+// save example html
+const save_html = (html) => {
     fs.writeFile('./examples/index.html', html, (err) => {
         if (err) throw err;
     })
 }
 
-
-inquirer
+const ask_questions = () => {
+    inquirer
     .prompt(questions)
     .then((answers) => {
-        if (answers.shape === 'Triangle'){
-            const triangle = new Triangle(answers.shape,answers.shapeColor, answers.text, answers.textColor);
-            html = triangle.html_generator();
-            save_html(html);
+
+        // shape color cannot be the same as text color, otherwise user cannot see text
+        if(answers.shapeColor===answers.textColor){
+            console.log('Shape color and text color cannot be the same. Please choose different colors.');
+            ask_questions();
+        }else{
+            if (answers.shape === 'Triangle'){
+                const triangle = new Triangle(answers.shape,answers.shapeColor, answers.text, answers.textColor);
+                html = triangle.html_generator();
+                save_html(html);
+            }
+            else if (answers.shape === 'Circle'){
+                const circle = new Circle(answers.shape,answers.shapeColor, answers.text, answers.textColor);
+                html = circle.html_generator();
+                save_html(html);
+            }
+            else if (answers.shape === 'Square'){
+                const square = new Square(answers.shape,answers.shapeColor, answers.text, answers.textColor);
+                html = square.html_generator();
+                save_html(html);
+            }
         }
-        else if (answers.shape === 'Circle'){
-            const circle = new Circle(answers.shape,answers.shapeColor, answers.text, answers.textColor);
-            html = circle.html_generator();
-            save_html(html);
-        }
-        else if (answers.shape === 'Square'){
-            const square = new Square(answers.shape,answers.shapeColor, answers.text, answers.textColor);
-            html = square.html_generator();
-            save_html(html);
-        }
+       
     })
+}
+
+ask_questions();
+
